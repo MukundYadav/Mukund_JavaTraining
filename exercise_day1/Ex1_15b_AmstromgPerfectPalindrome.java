faa4f1547d3459f58d8f6ad99686639c49b4cb5f
Mukund Yadav R N <mukundyadav.nagaraju@cdw.com>
2023-02-27 15:23:42 +0530

    Exercise-1 15b Updated

--- exercise_day1/Ex1_15b_AmstromgPerfectPalindrome.java
+++ exercise_day1/Ex1_15b_AmstromgPerfectPalindrome.java
@@ -3,16 +3,19 @@ package exercise_day1;
 import java.util.Scanner;
 
 public class Ex1_15b_AmstromgPerfectPalindrome {
+	static int c=0;
 	public static void main(String[] args) {
 		System.out.println("Enter the number.. ");
 		Scanner sc=new Scanner(System.in);
 		int num=sc.nextInt();
-		isAmstrong(num);
-		isPerfect(num);
-		isPalindrome(num);
+		checkAmstrong(num);
+		checkPerfect(num);
+		checkPalindrome(num);
+		if(c == 3)
+			System.out.println(num+" is none of the above!");
 		
 	}
-	public static void isAmstrong(int n){
+	public static void checkAmstrong(int n){
 		int digit,sum=0,count=0;
 		int g=n;
 		while(g!=0)
@@ -29,14 +32,13 @@ public class Ex1_15b_AmstromgPerfectPalindrome {
 		 if(sum==n)
 		 {System.out.println(n+" is an amstrong number");}
 		 else {
-			 System.out.println(n+" is not an amstrong number");
-		 } 
+			 //System.out.println(n+" is not an amstrong number");
+			 c++;
+		 }
 	}
 	
-	public static void isPerfect(int n) {
-		int sum=0;
-		 
-			 
+	public static void checkPerfect(int n) {
+		int sum=0; 
 		for(int i=1;i<n;i++) {
 			if(n%i==0)
 			 {
@@ -47,11 +49,12 @@ public class Ex1_15b_AmstromgPerfectPalindrome {
 			System.out.println(n+" is a perfect number");
 		}
 		else {
-			System.out.println(n+" is not a perfect number");
+			//System.out.println(n+" is not a perfect number");
+			 c++;
 		}
 	}
 	 
-	public static void isPalindrome(int k ) {
+	public static void checkPalindrome(int k ) {
 		int d,sum=0;
 		int n=k;
 		while(n!=0) {
@@ -64,7 +67,8 @@ public class Ex1_15b_AmstromgPerfectPalindrome {
 			System.out.println(k+" is a palindromic number");
 		}
 		else {
-			System.out.println(k+" is not a palindromic number");
+			//System.out.println(k+" is not a palindromic number");
+			 c++;
 		}
 	}
 }
