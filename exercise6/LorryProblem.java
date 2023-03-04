ae3dc09d5254eeb6e2ac7a119ce8462b4004b025
Mukund Yadav R N <mukundyadav.nagaraju@cdw.com>
2023-03-05 00:47:14 +0530

    Exercise-6 Second Commit(After Review Comments)

--- exercise6/LorryProblem.java
+++ exercise6/LorryProblem.java
@@ -3,10 +3,14 @@ package exercise6;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LorryProblem {
 	public static void main(String[] args) {
+
+		int driveTimePerDay = 8;
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Enter the speed of the Lorry (in km/hr): ");
@@ -15,38 +19,40 @@ public class LorryProblem {
 		System.out.print("Enter the distance to be travelled (in km): ");
 		double distance = scanner.nextDouble();
 
-		// System.out.print("Enter the starting date (in YYYY-MM-DD format): ");
-		// String startDateInput = scanner.next();
-		LocalDate startDate = LocalDate.now();
+		System.out.print("Enter the starting date (in YYYY-MM-DD format): ");
+		String startDateInput = scanner.next();
+		LocalDate startDate = LocalDate.parse(startDateInput);
 
-		// System.out.print("Enter the starting time (in HH:MM format): ");
-		// String startTimeInput = scanner.next();
-		LocalTime startTime = LocalTime.now();
+		System.out.print("Enter the starting time (in HH:MM format): ");
+		String startTimeInput = scanner.next();
+		LocalTime startTime = LocalTime.parse(startTimeInput);
 
 		scanner.close();
 
 		double totalTimeRequired = distance / speed;
 
-		double totalNumberOfDays = Math.floor(totalTimeRequired / 8);
+		double totalNumberOfDays = Math.ceil(totalTimeRequired / driveTimePerDay);
 
-		int totalHolidays = 0;
-		for (int i = 0; i < totalNumberOfDays; i++) {
+		List<String> holydays = new ArrayList<>();
+
+		for (int i = 0; i <= totalNumberOfDays; i++) {
+
 			LocalDate currentDate = startDate.plusDays(i);
-
-			if (currentDate.getDayOfWeek().getValue() != 7 && currentDate.getDayOfWeek().getValue() != 6) { 							// Saturday
-				if (currentDate.getMonthValue() == 1 && currentDate.getDayOfMonth() == 1
-						|| currentDate.getMonthValue() == 1 && currentDate.getDayOfMonth() == 26
-						|| currentDate.getMonthValue() == 8 && currentDate.getDayOfMonth() == 15) {
-					totalHolidays++;
-				} else if (currentDate.getDayOfWeek().getValue() == 1) { 
-					if (currentDate.plusDays(6).getMonthValue() == currentDate.getMonthValue()) { 
-						totalHolidays++;
-					}
-				}
+			if (currentDate.getMonthValue() == 1 && currentDate.getDayOfMonth() == 1) {
+				holydays.add("New Year Day");
+			} else if (currentDate.getMonthValue() == 1 && currentDate.getDayOfMonth() == 26) {
+				holydays.add("Republic Day");
+			} else if (currentDate.getMonthValue() == 8 && currentDate.getDayOfMonth() == 15) {
+				holydays.add("Independence Day");
+			} else if (currentDate.getDayOfWeek().getValue() == 6 && currentDate.getDayOfMonth() >= 8
+					&& currentDate.getDayOfMonth() <= 14) {
+				holydays.add("Second Saturday");
+			} else if (currentDate.getDayOfWeek().getValue() == 7) {
+				holydays.add("Sunday");
 			}
 		}
 
-		double totalNumDays = totalNumberOfDays + totalHolidays;
+		double totalNumDays = totalNumberOfDays + holydays.size();
 
 		LocalDate destinationDate = startDate.plusDays((long) totalNumDays);
 		LocalTime destinationTime = startTime.plus((long) Math.floorMod(Math.round(totalTimeRequired * 60), 480),
